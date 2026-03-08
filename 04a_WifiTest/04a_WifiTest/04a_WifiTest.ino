25916f47d0c2fd1c1c3ae6c822e9b1b6d5414739
Julio Garcia-Rodriguez <betoti_23@hotmail.com>
2026-03-07 22:05:46 -0600

    Add 04a_Wifi Test

---
+++ 04a_WifiTest/04a_WifiTest/04a_WifiTest.ino
@@ -0,0 +1,49 @@
+/*  Microcontrollers 2026
+    J.A. García.Rodríguez
+    WiFi Test for ESP32-S3.
+
+ * This sketch test ESP32-S3 Wifi conectivity through the SSID and password. When the Dev-Board is correctly connected to the the Wifi  
+ * prints a local IP address. You can access this local IP and verify the message send by the ESP32-S3
+*/
+
+#include <WiFi.h>             
+#include <WebServer.h>
+
+WebServer server(80);
+
+void setup() {
+  Serial.begin(115200);
+  const char* ssid     = "IZZI-9EB9";                        // Wifi
+  const char* password = "F0AF85519EB9";                     // Password
+  
+  Serial.println("Star Wifi Conectivity");
+  WiFi.disconnect();
+  
+  Serial.print("Conected yo  ");
+  Serial.println(ssid);
+  
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+  
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  
+  Serial.println("");
+  Serial.println("WiFi Successfully Conected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+  
+  server.on("/", message);
+  server.begin();
+}
+void loop() {
+  server.handleClient();
+  delay(100);
+}
+
+void message()  
+{
+  server.send(200, "text/html", "<h2> This message is from the ESP32-S3. <br> 2026 MICROCONTROLLERS </h2>");
+}
